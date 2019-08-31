b433caf4dd14716b3d672363f6909f0c2c7f1047
Jason Chen <jason.sl.chen@gmail.com>
2019-08-31 13:31:04 -0700

    date formatting fixed

--- client/dateModule.jsx
+++ client/dateModule.jsx
@@ -8,6 +8,9 @@ font-family: Brandon-Text-Regular;
 width: 50%;
 box-sizing: border-box;
 margin-right: 2.5%;
+display: flex;
+flex-direction: column;
+justify-content: flex-start;
 `;
 
 const DateSelect = styled.select`
@@ -27,33 +30,63 @@ margin: none;
 outline: none;
 `;
 
+const DateSelectDiv = styled.div`
+position: absolute;
+margin: none;
+width: 39%;
+align-self: center;
+padding-top: 5px;
+`;
+
 const DateTitle = styled.div`
 height: 50%;
 margin: none;
+`;
+
+const DateDisplayWrap = styled.div`
+pointer-events: none
 display: flex;
+flex-direction: row;
+position: absolute;
+box-sizing: border-box;
+margin: none;
+justify-content: space-between;
+width: 39%;
+align-self: center;
 `;
 
 const DateDisplay = styled.div`
 box-sizing: border-box;
-width: 82%;
+width: 50%;
 align-items: center;
-position: absolute;
+align-self: flex-start;
 pointer-events: none;
 font-size: 85%;
-margin-left: auto;
-margin-right: auto;
+margin: none;
 font-family: Brandon-Text-Light;
+box-sizing: border-box;
 `;
 
-// const DropDownIcon = styled.svg`
-// width: 8px;
-// height: 34px;
-// // align-self: flex-end;
-// `;
+const DropDownDiv = styled.div`
+align-self: flex-end;
+height: 100%;
+`;
 
-// const DropDownPath = styled.path`
-// fill: rgb(51, 51, 51);
-// `;
+const DropDownIcon = styled.svg`
+width: 8px;
+padding-bottom: 5px;
+`;
+
+const DateDisplayFinal = styled.div`
+width: 100%;
+margin: none;
+box-sizing: border-box;
+`;
+
+
+const DropDownPath = styled.path`
+fill: rgb(51, 51, 51);
+`;
 
 class DateModule extends React.Component {
   constructor(props) {
@@ -83,10 +116,18 @@ class DateModule extends React.Component {
     return (
       <DateDiv>
         <DateTitle>Date</DateTitle>
-        <DateDisplay>{this.state.shownDate}</DateDisplay>
-        {/* <DropDownIcon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8.07 5.24"><DropDownPath d="M4.39 5.09l.71-.71 2.82-2.82a.5.5 0 0 0 0-.71l-.7-.7a.5.5 0 0 0-.71 0L4 2.62 1.56.15a.5.5 0 0 0-.71 0l-.7.7a.5.5 0 0 0 0 .71L3 4.39l.71.71a.5.5 0 0 0 .68-.01z" /></DropDownIcon> */}
-        <DateSelect onClick={this.openCalender} />
-        {this.state.calender ? <CalenderModule back={this.props.back} next={this.props.next} openCalender={this.openCalender} changeShownDate={this.changeShownDate} selectDate={this.props.selectDate} month={this.props.month} /> : ''}
+        <DateDisplayFinal>
+          <DateSelectDiv>
+            <DateSelect onClick={this.openCalender} />
+            {this.state.calender ? <CalenderModule back={this.props.back} next={this.props.next} openCalender={this.openCalender} changeShownDate={this.changeShownDate} selectDate={this.props.selectDate} month={this.props.month} /> : ''}
+          </DateSelectDiv>
+          <DateDisplayWrap>
+            <DateDisplay>{this.state.shownDate}</DateDisplay>
+            <DropDownDiv>
+              <DropDownIcon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8.07 5.24"><DropDownPath d="M4.39 5.09l.71-.71 2.82-2.82a.5.5 0 0 0 0-.71l-.7-.7a.5.5 0 0 0-.71 0L4 2.62 1.56.15a.5.5 0 0 0-.71 0l-.7.7a.5.5 0 0 0 0 .71L3 4.39l.71.71a.5.5 0 0 0 .68-.01z" /></DropDownIcon>
+            </DropDownDiv>
+          </DateDisplayWrap>
+        </DateDisplayFinal>
       </DateDiv>
     );
   }
