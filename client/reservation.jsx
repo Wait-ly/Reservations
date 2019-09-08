81a0db37590dd391ebefddc960663b0709c67af1
Jason Chen <jason.sl.chen@gmail.com>
2019-09-07 18:49:58 -0700

    change zheight of module

--- client/reservation.jsx
+++ client/reservation.jsx
@@ -62,7 +62,7 @@ MongoNetworkError: failed to connect to server [database:27017] on first connect
 `;
 
 const Reservation = styled.div`
-z-index: 5000;
+z-index: 1000;
 position: fixed;
 box-sizing: border-box;
 display: flex;
